d3cf387577ac101cb1a854235cc060a133f981b5
neraaaam <mary_dietrich@gmx.de>
2016-11-02 11:41:56 +0100

    Erweiterung Population, Fitness komisch

--- MLE/src/Genstrang.java
+++ MLE/src/Genstrang.java
@@ -1,51 +1,58 @@
 
 public class Genstrang {
-
-	public static int[] genstrang = new int[5];
-
-	public static int[] bestGenstrang = new int[5];
-
-	public static void fillGenstrang() {
-		for (int i = 0; i < genstrang.length; i++) {
-			genstrang[i] = (Math.random() < 0.5) ? 0 : 1;
-		}
-		for (int j = 0; j < genstrang.length; j++) {
-			System.out.print(genstrang[j]);
-		}
-		System.out.println();
-		
-	}
 	
-	public static void fillBestGenstrang() {
-		for (int i = 0; i < bestGenstrang.length; i++) {
-			bestGenstrang[i] = (Math.random() < 0.5) ? 0 : 1;
-		}
-		for (int j = 0; j < bestGenstrang.length; j++) {
-			System.out.print(bestGenstrang[j]);			
-		}
-		System.out.println();
-		
-	}
+	public static int populationSize = 5;
+	public static int geneSize = 5;
 	
+	//geneSize +1 because in the first field of each individual we save its fitness
+	public static int[][] population = new int[populationSize][geneSize+1];
 
-	public static int calculateFitness(int mutation[]) {
+	public static int[] targetGenstrang = new int[5];
 
-		int fitness = 0;
-		for (int i = 0; i < genstrang.length; i++) {
-			if (genstrang[i] == mutation[i]) {
-				fitness += 1;
+	public static void fillPopulation() {
+		for (int i = 0; i < population.length; i++) {
+			for(int k = 1; k < population[i].length; k++){
+				population[i][k] = (Math.random() < 0.5) ? 0 : 1;
 			}
 		}
-		System.out.println(fitness);
+		HillClimber.print2DArray(population);
+		
+	}
+	
+	public static void fillTargetGenstrang() {
+		for (int i = 0; i < targetGenstrang.length; i++) { 
+			targetGenstrang[i] = (Math.random() < 0.5) ? 0 : 1;
+		}
+		for (int j = 0; j < targetGenstrang.length; j++) {
+			System.out.print(targetGenstrang[j]);			
+		}
+		System.out.println();
+		
+	}
+	
+
+	public static int calculateFitness() {
+		int fitness = 0;
+		for (int i = 0; i < population.length; i++) {
+			fitness = 0;
+			for(int k = 1; k < population[i].length; k++){
+				if (population[i][k] == targetGenstrang[k-1]){
+					fitness += 1;
+				}
+			}
+			population[i][0]= fitness;
+			System.out.println("Fitness of individual " +i +": " +fitness);
+		}
+
 		return fitness;
 		
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Genstrang.fillGenstrang();
-		Genstrang.fillBestGenstrang();
-		calculateFitness(bestGenstrang);
+		Genstrang.fillPopulation();
+		Genstrang.fillTargetGenstrang();
+		calculateFitness();
 	}
 
 }
